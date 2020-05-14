2d0a1e5c40bde4e7dac85c1c48fe2a70085123af
ZHI LI <zl2401@columbia.edu>
2020-05-14 14:40:59 -0400

    modify palindrome

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,6 +1,6 @@
 public class Palindrome {
     public Deque<Character> wordToDeque(String word) {
-        LinkedListDeque<Character> deque = new LinkedListDeque<>();
+        Deque<Character> deque = new LinkedListDeque<>();
         for (int i = 0; i < word.length(); i++) {
             deque.addLast(word.charAt(i));
         }
